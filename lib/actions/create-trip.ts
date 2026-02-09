1e81ed31cabbe32933b95cf6db4984afcc547ce4
Liam <105169355+Cricket-Programming@users.noreply.github.com>
2026-02-08 19:24:49 -0600

    Added Extra stuff to create trip logic?

--- lib/actions/create-trip.ts
+++ lib/actions/create-trip.ts
@@ -1,9 +1,18 @@
+import { auth } from "@/auth";
+import { prisma } from "../prisma";
+import { redirect } from "next/navigation";
+
 export async function createTrip(formData: FormData) {
 
     const session = await auth();
-    if (!session) {
+
+    // basically error handling!
+    if (!session || !session.user?.id) {
         throw new Error("Not Authenticated.");
     }
+
+    // ? is the optional chaining operator; it says that if formData.get() is null, stop and make title undefined. 
+
     const title = formData.get("title")?.toString();
     const description = formData.get("description")?.toString();
     const startDateStr = formData.get("startDate")?.toString();
@@ -17,16 +26,15 @@ export async function createTrip(formData: FormData) {
     const startDate = new Date(startDateStr);
     const endDate = new Date(endDateStr);
 
-    await prisma.trip.create(   
+    await prisma.trip.create({
         data: {
             title,
             description,
             startDate,
             endDate,
-            userId: session.user?.id
-        }
-    })
+            userId: session.user.id,
+        },
+    });
 
-    // ? is the optional chaining operator; it says that if formData.get() is null, stop and make title undefined.
-    //  If not, set the value for title as  . 
+    redirect("/trips");
 }
