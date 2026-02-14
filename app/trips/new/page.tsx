b9abffde61f86f5d4f84a4c706b65f02e92f2bbb
Liam <105169355+Cricket-Programming@users.noreply.github.com>
2026-02-14 14:52:46 -0600

    added functionality for trip saving

--- app/trips/new/page.tsx
+++ app/trips/new/page.tsx
@@ -1,15 +1,32 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import Image  from "next/image"; // default import from default function, I can name it whatever I want
+import { Button } from "@/components/ui/button"; // 
 import { Card, CardHeader, CardContent } from "@/components/ui/card";
+import { createTrip } from "@/lib/actions/create-trip";
 import { cn } from "@/lib/utils";
+import { UploadButton } from "@/lib/upload-thing"
+import { useState, useTransition } from "react";
 
 export default function NewTrip() {
+    // console.log("Test123"); // this would be an example of a client-side log
+    const [isPending, startTransition] = useTransition();
+    const [imageUrl, setImageUrl] = useState<string | null>(null);    // data passed in as props come from other components
     return (<div className="max-w-lg mx-auto mt-15">
+
+
         <Card>
-            <CardHeader>New Trip</CardHeader>
+            <CardHeader>New Assignment</CardHeader>
             <CardContent>
-                <form className="space-y-6">
+                <form
+                    className="space-y-6"
+                    action={(formData: FormData) => {
+                        if (imageUrl) {
+                            formData.append("imageUrl", imageUrl);
+                        }
+                        startTransition(() => createTrip(formData));
+                    }}
+                >
                     <div>
                         <label className="block text-sm font-medium text-gray-700 mb-1">Title</label>
                         <input 
@@ -26,7 +43,7 @@ export default function NewTrip() {
                     <div>
                         <label className="block text-sm font-medium text-gray-700 mb-1">Description</label>
                         <textarea 
-                            name="Description" 
+                            name="description" 
                             placeholder="Saw three of the most important landmarks over 7 days..."
                             className={cn(
                                 "w-full border border-gray border-gray-300 px-2 py-2",
@@ -57,7 +74,7 @@ export default function NewTrip() {
                             </label>
                             <input
                                 type="date"
-                                name="enddate" 
+                                name="endDate" 
                                 className={cn(
                                     "w-full border border-gray border-gray-300 px-2 py-2",
                                     "rounded-md focus:outline-none focus:ring-2 focus:ring-pink-800"
@@ -66,10 +83,38 @@ export default function NewTrip() {
                             />
                         </div>
                     </div>
-                    {/* <Link> */}
-                        <Button type="submit" className="w-full">Create Trip</Button>
-                    {/* </Link> */}
 
+                    <div>
+                        <label>Trip Image</label>
+
+                        {imageUrl && (
+                            <Image 
+                                src={imageUrl} 
+                                alt="Trip Preview"
+                                className="w-full mb-4 max-h-48"
+                                width={300}
+                                height={100}
+                                
+                            />
+                        )}
+                        <UploadButton
+                            endpoint="imageUploader"
+                            onClientUploadComplete={(res) => {
+                                if (res && res[0].ufsUrl) {
+                                    setImageUrl(res[0].ufsUrl);
+                                }
+                            }}
+
+                            onUploadError={(error: Error) => {
+                                console.log("Upload Error", error)
+                            }}
+                        />
+                    </div>
+                    
+                        {/* The syntax disabled={isPending} makes sense here. disabled is the attribute {} allows you to pass in variables  */}
+                        <Button type="submit" disabled={isPending} className="w-full">
+                            {isPending ? "Creating..." : "Create Trip"}
+                        </Button> 
                 </form>
             </CardContent>
         </Card>
